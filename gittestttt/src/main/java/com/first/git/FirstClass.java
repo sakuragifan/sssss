c1485aad7c738ef96178df56363b7e4a06930485
Fan <609485623@qq.com>
2019-07-10 17:00:12 +0800

    本地工程添加到git

---
+++ gittestttt/src/main/java/com/first/git/FirstClass.java
@@ -0,0 +1,11 @@
+package com.first.git;
+
+public class FirstClass {
+
+    public static void main(String[] args) {
+
+        System.out.println("ssssss");
+        System.out.println("11111");
+    }
+
+}
